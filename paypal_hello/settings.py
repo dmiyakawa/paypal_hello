09a17860391cb8ceb830ac564bc1b480dd48a095
Daisuke Miyakawa <d.miyakawa@gmail.com>
2014-09-06 15:44:10 +0900

    Move db.sqlite3 to db/

--- paypal_hello/settings.py
+++ paypal_hello/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'paypal_hello.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
